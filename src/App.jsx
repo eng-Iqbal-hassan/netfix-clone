ba62565ae7bd741ee7b0168942e99d74df6a10bf
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2024-11-06 19:08:18 +0500

    browser router is added and sign-up page structure is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      
     </div>
   )
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
     <div className='log-in'>
-      this is log-in page
+      <img src={logo} alt="" className='login-logo' />
+      <div>
+        <h2>Sign Up</h2>
+        <form action="">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
