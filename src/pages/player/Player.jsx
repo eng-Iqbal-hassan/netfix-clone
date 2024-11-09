6470df0e2109d0f6d7b9bfba1cfef4e18d45f476
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2024-11-09 23:50:09 +0500

    player page structure is added

--- src/pages/player/Player.jsx
+++ src/pages/player/Player.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='player'>
-      this is player page
+      <img src={back_arrow_icon} alt="" />
+      <iframe width='90%' height='90%' src="https://www.youtube.com/embed/xHqrDqjMi5g"title='trailer' frameborder="0" allowFullScreen></iframe>
+      <div className="player-info">
+        <p>Published Name</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
     </div>
   )
 }
